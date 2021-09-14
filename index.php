aee11550f4c88bea52f70038135120d3111b8bdf
Aissyla <87979885+Aissyla@users.noreply.github.com>
2021-09-14 16:01:30 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,35 +1,84 @@
 <?php
+// On démarre une session
+session_start();
 
-//P.O.O programmation orienté objet
-class Connect
-{
-    private $servername;
-    private $username;
-    private $dbname;
-    private $password;
-    private $connect;
+// On inclut la connexion à la base
+require_once('connect.php');
 
-    public function __construct($servername, $username, $password, $dbname)
-    {
-        $this->servername = $servername;
-        $this->username = $username;
-        $this->dbname = $dbname;
-        $this->password = $password;
-    }
+$sql = 'SELECT * FROM `liste`';
 
-    public function meconnecterdb()
-    {
-        return $this->connect = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
-    }
-}
+// On prépare la requête
+$query = $db->prepare($sql);
 
-// définir connexion
-$connection = new Connect("localhost", "root", "", "crud");
+// On exécute la requête
+$query->execute();
 
-// afficher connexion
-echo  "<pre>";
-//var_dump($connection->meconnecterdb());
-echo "</pre>";
+// On stocke le résultat dans un tableau associatif
+$result = $query->fetchAll(PDO::FETCH_ASSOC);
 
-// variable réutilisable class Connect
-//$meconnecter=$connection->meconnecterdb();
+require_once('close.php');
+?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Liste des produits</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+</head>
+
+<body>
+    <main class="container">
+        <div class="row">
+            <section class="col-12">
+                <?php
+                if (!empty($_SESSION['erreur'])) {
+                    echo '<div class="alert alert-danger" role="alert">
+                                ' . $_SESSION['erreur'] . '
+                            </div>';
+                    $_SESSION['erreur'] = "";
+                }
+                ?>
+                <?php
+                if (!empty($_SESSION['message'])) {
+                    echo '<div class="alert alert-success" role="alert">
+                                ' . $_SESSION['message'] . '
+                            </div>';
+                    $_SESSION['message'] = "";
+                }
+                ?>
+                <h1>Liste des produits</h1>
+                <table class="table">
+                    <thead>
+                        <th>ID</th>
+                        <th>Produit</th>
+                        <th>Prix</th>
+                        <th>Nombre</th>
+                        <th>Actions</th>
+                    </thead>
+                    <tbody>
+                        <?php
+                        // On boucle sur la variable result
+                        foreach ($result as $produit) {
+                        ?>
+                            <tr>
+                                <td><?= $produit['id'] ?></td>
+                                <td><?= $produit['produit'] ?></td>
+                                <td><?= $produit['prix'] ?></td>
+                                <td><?= $produit['nombre'] ?></td>
+                                <td><a href="details.php?id=<?= $produit['id'] ?>">Voir</a> <a href="edit.php?id=<?= $produit['id'] ?>">Modifier</a> <a href="delete.php?id=<?= $produit['id'] ?>">Supprimer</a></td>
+                            </tr>
+                        <?php
+                        }
+                        ?>
+                    </tbody>
+                </table>
+                <a href="add.php" class="btn btn-primary">Ajouter un produit</a>
+            </section>
+        </div>
+    </main>
+</body>
+
+</html>
